0b7771fd0dd6edf1372ba06d368f5f9539f0dba3
Huu Ty <typrone1@gmail.com>
2018-07-25 21:21:41 +0700

    Commit code

---
+++ app/src/main/java/com/tynh/studentmanagement/AZSearchActivity.java
@@ -0,0 +1,146 @@
+package com.tynh.studentmanagement;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class AZSearchActivity extends AppCompatActivity {
+    private ListView sortListView;
+    private SideBar sideBar;
+    private TextView dialog;
+    private SortAdapter adapter;
+    private ClearEditText mClearEditText;
+    private CharacterParser characterParser;
+    private List<SortModel> SourceDateList;
+    private PinyinComparator pinyinComparator;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_azsearch);
+        initViews();
+    }
+
+    private void initViews() {
+
+        characterParser = CharacterParser.getInstance();
+
+        pinyinComparator = new PinyinComparator();
+
+        sideBar = (SideBar) findViewById(R.id.sidrbar);
+        dialog = (TextView) findViewById(R.id.dialog);
+        sideBar.setTextView(dialog);
+
+
+        //ﾉ靹ﾃﾓﾒｲ犇･ﾃ�ｼ猯�
+        sideBar.setOnTouchingLetterChangedListener(new SideBar.OnTouchingLetterChangedListener() {
+
+            @Override
+            public void onTouchingLetterChanged(String s) {
+                //ｸﾃﾗﾖﾄｸﾊﾗｴﾎｳﾖｵﾄﾎｻﾖﾃ
+                int position = adapter.getPositionForSection(s.charAt(0));
+                if(position != -1){
+                    sortListView.setSelection(position);
+                }
+
+            }
+        });
+
+        sortListView = (ListView) findViewById(R.id.country_lvcountry);
+        sortListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Toast.makeText(getApplication(), ((SortModel) adapter.getItem(position)).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        SourceDateList = filledData(getResources().getStringArray(R.array.date));
+
+
+        Collections.sort(SourceDateList, pinyinComparator);
+        adapter = new SortAdapter(this, SourceDateList);
+        sortListView.setAdapter(adapter);
+
+
+        mClearEditText = (ClearEditText) findViewById(R.id.filter_edit);
+
+
+        mClearEditText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                filterData(s.toString());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+
+    private List<SortModel> filledData(String[] date) {
+        List<SortModel> mSortList = new ArrayList<SortModel>();
+
+        for (int i = 0; i < date.length; i++) {
+            SortModel sortModel = new SortModel();
+            sortModel.setName(date[i]);
+
+            String pinyin = characterParser.getSelling(date[i]);
+            String sortString = pinyin.substring(0, 1).toUpperCase();
+
+
+            if (sortString.matches("[A-Z]")) {
+                sortModel.setSortLetters(sortString.toUpperCase());
+            } else {
+                sortModel.setSortLetters("#");
+            }
+
+            mSortList.add(sortModel);
+        }
+        return mSortList;
+
+    }
+
+
+    private void filterData(String filterStr) {
+        List<SortModel> filterDateList = new ArrayList<SortModel>();
+
+        if (TextUtils.isEmpty(filterStr)) {
+            filterDateList = SourceDateList;
+        } else {
+            filterDateList.clear();
+            for (SortModel sortModel : SourceDateList) {
+                String name = sortModel.getName();
+                if (name.indexOf(filterStr.toString()) != -1 || characterParser.getSelling(name).startsWith(filterStr.toString())) {
+                    filterDateList.add(sortModel);
+                }
+            }
+        }
+
+
+        Collections.sort(filterDateList, pinyinComparator);
+        adapter.updateListView(filterDateList);
+    }
+}
--- app/src/main/java/com/tynh/studentmanagement/MainActivity.java
+++ app/src/main/java/com/tynh/studentmanagement/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.tynh.studentmanagement.R;
 
 public class MainActivity extends AppCompatActivity {
     TextView textView1;
